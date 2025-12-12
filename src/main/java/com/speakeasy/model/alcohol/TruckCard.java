eca2392b278bebdbaac65d495135c7325044e237
s2411613-netizen <s2411613@gl.cc.uec.ac.jp>
2025-12-12 16:01:14 +0900

    Update TruckCard.java

--- src/main/java/com/speakeasy/model/alcohol/TruckCard.java
+++ src/main/java/com/speakeasy/model/alcohol/TruckCard.java
@@ -1,2 +1,25 @@
-// トラックカード
+package com.speakeasy.model.alcohol;
+
+/**
+ * TruckCard
+ * ----------------------------------------
+ * 複数の AlcoholCard をまとめて保持するデータ構造。
+ *
+ * 今回は1セット12本の酒をまとめた構造として利用する。
+ *
+ */
+public class TruckCard {
+
+    /** このトラックに積まれているお酒のリスト */
+    private final List<AlcoholCard> alcohols;
+
+    public TruckCard(List<AlcoholCard> alcohols) {
+        this.alcohols = alcohols;
+    }
+
+    /** トラックに積まれた酒一覧を返す（読み取り用） */
+    public List<AlcoholCard> getAlcohols() {
+        return alcohols;
+    }
+}
 
