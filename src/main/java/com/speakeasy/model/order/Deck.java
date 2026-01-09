a9ee549be71728259281dc10ded3db8a08d0f33c
s2411613 <s2411613@gl.cc.uec.ac.jp>
2026-01-09 16:19:59 +0900

    edit Deck.java

--- src/main/java/com/speakeasy/model/order/Deck.java
+++ src/main/java/com/speakeasy/model/order/Deck.java
@@ -1,1 +1,32 @@
+papackage com.speakeasy.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Deck
+ * ------------------------------
+ * カードの山札を表すクラス。
+ *
+ * ・シャッフルする
+ * ・カードを1枚引く
+ *
+ */
+
+public class Deck<T> {
+
+    // 山札の中身を追加
+    private List<T> cards = new arrayList<>(); 
+
+    public void add(T card) {
+        cards.add(card);
+    }
+
+    public void addAll(List<T> list) {
+        cards.addAll(list);
+    }
+
+    // 山札をシャッフル
+    
+ }
